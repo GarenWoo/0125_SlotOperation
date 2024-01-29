2137df3ec4632b31ecfba5676407b999ce2e8e13
Garen Woo <351394974@qq.com>
2024-01-30 02:49:31 +0800

    Updated Contract Codes

--- contracts/NFTMarket_V2_2.sol
+++ contracts/NFTMarket_V2_2.sol
@@ -7,9 +7,19 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-interface IERC721TokenWithPermit {
-    function NFTPermit(
+interface NFTMarket_Standard_Interface_V1 {
+    function NFTPermit_PrepareForBuy(
         address,
+        uint256,
+        uint256,
+        uint8,
+        bytes32,
+        bytes32
+    ) external returns (bool);
+
+    function NFTPermit_PrepareForList(
+        address,
+        uint256,
         uint256,
         uint256,
         uint8,
@@ -23,12 +33,14 @@ interface IERC721TokenWithPermit {
  *
  * @author Garen Woo
  */
-contract NFTMarket_V2_1 is IERC721Receiver {
+contract NFTMarket_V2_2 is IERC721Receiver {
     address private owner;
-    mapping(address => mapping(uint => uint)) private price;
-    mapping(address => uint) private balance;
+    mapping(address tokenAddress => mapping(uint tokenId => uint price))
+        private price;
+    mapping(address userAddress => uint balance) private balance;
     address public immutable tokenAddr;
-    mapping(address => mapping(uint => bool)) public onSale;
+    mapping(address tokenAddress => mapping(uint tokenId => bool isOnSale))
+        public onSale;
     event NFTListed(address NFTAddr, uint256 tokenId, uint price);
     event NFTDelisted(address NFTAddr, uint256 tokenId);
     event NFTBought(address NFTAddr, uint256 tokenId, uint bidValue);
@@ -69,6 +81,13 @@ contract NFTMarket_V2_1 is IERC721Receiver {
         return this.onERC721Received.selector;
     }
 
+    /**
+     * @notice Once this function is called, the 'msg.sender' will try to buy NFT with the token transferred.
+     * The NFT address and tokenId of the NFT separately come from `nftAddress` and 'tokenId', which are decoded from the `data` in the input list.
+     *
+     * @dev Important! If your NFT project supports the function of buying NFT with off-chain signature of messages(i.e.permit), make sure the NFT contract(s) should have realized NFTMarket_Standard_Interface_V1.
+     * Without the realization of NFTMarket_Standard_Interface_V1, malevolent EOAs can directly buy NFTs without permit-checking.
+     */
     function tokensReceived(
         address _recipient,
         uint _amount,
@@ -81,8 +100,10 @@ contract NFTMarket_V2_1 is IERC721Receiver {
             tokenId,
             _amount
         );
-        bool isERC721Permit = _isERC721PermitToken(nftAddress);
-        if (isERC721Permit) {
+        bool hasNFTMarketStandardInterface = _support_NFTMarketStandardInterface(
+                nftAddress
+            );
+        if (hasNFTMarketStandardInterface) {
             revert ERC721PermitBoughtByWrongFunction("buy", "buyWithPermit");
         }
         if (checkResult) {
@@ -104,16 +125,7 @@ contract NFTMarket_V2_1 is IERC721Receiver {
             onSale[_nftAddr][_tokenId] == false,
             "This NFT is already listed"
         );
-        IERC721(_nftAddr).safeTransferFrom(
-            msg.sender,
-            address(this),
-            _tokenId,
-            "List successfully"
-        );
-        IERC721(_nftAddr).approve(msg.sender, _tokenId);
-        price[_nftAddr][_tokenId] = _price;
-        onSale[_nftAddr][_tokenId] = true;
-        emit NFTListed(_nftAddr, _tokenId, _price);
+        _List(_nftAddr, _tokenId, _price);
     }
 
     /// @dev The seller(EOA), is the owner of the NFT when it was not listed.
@@ -134,9 +146,12 @@ contract NFTMarket_V2_1 is IERC721Receiver {
         emit NFTDelisted(_nftAddr, _tokenId);
     }
 
-    /* 
-        Directly Buy NFT without checking if the msg.sender is in the white list of ERC721Token 
-    */
+    /**
+     * @notice Directly Buy NFT without checking ERC721 token permit.
+     *
+     * @dev Important! If your NFT project supports the function of buying NFT with off-chain signature of messages(i.e.permit), make sure the NFT contract(s) should have realized NFTMarket_Standard_Interface_V1.
+     * Without the realization of NFTMarket_Standard_Interface_V1, malevolent EOAs can directly buy NFTs without permit-checking.
+     */
     function buy(address _nftAddr, uint256 _tokenId, uint _bidValue) external {
         bool checkResult = _beforeUpdateNFT(
             msg.sender,
@@ -144,8 +159,10 @@ contract NFTMarket_V2_1 is IERC721Receiver {
             _tokenId,
             _bidValue
         );
-        bool isERC721Permit = _isERC721PermitToken(_nftAddr);
-        if (isERC721Permit) {
+        bool hasNFTMarketStandardInterface = _support_NFTMarketStandardInterface(
+                _nftAddr
+            );
+        if (hasNFTMarketStandardInterface) {
             revert ERC721PermitBoughtByWrongFunction("buy", "buyWithPermit");
         }
         if (checkResult) {
@@ -172,14 +189,15 @@ contract NFTMarket_V2_1 is IERC721Receiver {
             _tokenId,
             _bidValue
         );
-        bool isPermitVerified = IERC721TokenWithPermit(_nftAddr).NFTPermit(
-            msg.sender,
-            _tokenId,
-            _deadline,
-            _v,
-            _r,
-            _s
-        );
+        bool isPermitVerified = NFTMarket_Standard_Interface_V1(_nftAddr)
+            .NFTPermit_PrepareForBuy(
+                msg.sender,
+                _tokenId,
+                _deadline,
+                _v,
+                _r,
+                _s
+            );
         if (checkResult && isPermitVerified) {
             _updateNFT(msg.sender, _nftAddr, _tokenId, _bidValue);
             emit NFTBoughtWithPermit(_nftAddr, _tokenId, _bidValue);
@@ -209,7 +227,7 @@ contract NFTMarket_V2_1 is IERC721Receiver {
      */
     function getOwnerOfNFTMarket() public view returns (address ownerAddress) {
         assembly {
-            ownerAddress := sload(0)    
+            ownerAddress := sload(0)
         }
     }
 
@@ -224,14 +242,28 @@ contract NFTMarket_V2_1 is IERC721Receiver {
         return isApproved;
     }
 
-    function _isERC721PermitToken(
+    function _support_NFTMarketStandardInterface(
         address _nftAddr
     ) internal view returns (bool) {
-        bytes4 IERC721TokenWithPermit_Id = type(IERC721TokenWithPermit)
-            .interfaceId;
-        IERC165 ERC721PermitContract = IERC165(_nftAddr);
+        bytes4 NFTMarket_Standard_Interface_V1_Id = type(
+            NFTMarket_Standard_Interface_V1
+        ).interfaceId;
+        IERC165 NFTContract = IERC165(_nftAddr);
         return
-            ERC721PermitContract.supportsInterface(IERC721TokenWithPermit_Id);
+            NFTContract.supportsInterface(NFTMarket_Standard_Interface_V1_Id);
+    }
+
+    function _List(address _nftAddr, uint256 _tokenId, uint _price) internal {
+        IERC721(_nftAddr).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _tokenId,
+            "List successfully"
+        );
+        IERC721(_nftAddr).approve(msg.sender, _tokenId);
+        price[_nftAddr][_tokenId] = _price;
+        onSale[_nftAddr][_tokenId] = true;
+        emit NFTListed(_nftAddr, _tokenId, _price);
     }
 
     function _beforeUpdateNFT(
